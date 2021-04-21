0a6f36eeb7f849a756684161bc9a0d93d20b89b1
Your Name <you@example.com>
2021-04-21 13:36:49 +0600

    home work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import counterReducer from './redux/reduces'
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+
+const store = createStore(counterReducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +21,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
---
+++ src/redux/reduces.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+const initialState = {
+  number: 0,
+}
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+      case "increment_by_1":
+          return { ...state, number: state.number + 1 }
+      case "decrement_by_1":
+          return { ...state, number: state.number - 1 }
+      case "increment_by_10":
+          return { ...state, number: state.number + 10 }
+      case "decrement_by_10":
+          return { ...state, number: state.number - 10 }
+      case "multiply_by_2":
+          return { ...state, number: state.number * 2 }
+      case "divide_by_2":
+          return { ...state, number: state.number / 2 }
+    //   case "random":
+    //     return Math.floor(Math.random() * max);
+
+      case "canoutto":
+        return { ...state, number: state.number = 0 }
+        
+
+  }
+  
+  return state;
+}
+
+export default counterReducer;
